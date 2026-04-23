cb5a0cf6af9f4c44c6be2f8f126f07c1eea11511
Agbebi Timothy <agbebitimothy8@gmail.com>
2026-04-23 17:43:07 +0100

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb+srv://agbebitimothy8_db_user:Tims2000@tims.kjghuix.mon
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://e-commerce-frontend-seven-sage.vercel.app/",
         methods: ['GET', 'POST', 'DELETE', 'PUT'],
         allowedHeaders: [
             'Content-Type',
