15aa0d5519d585796e97405a4482aea0393fdcc7
Agbebi Timothy <agbebitimothy8@gmail.com>
2026-04-24 10:20:18 +0100

    a

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb+srv://agbebitimothy8_db_user:Tims2000@tims.kjghuix.mon
 
 app.use(
     cors({
-        origin: "https://e-commerce-frontend-seven-sage.vercel.app",
+        origin: "*",
         methods: ['GET', 'POST', 'DELETE', 'PUT'],
         allowedHeaders: [
             'Content-Type',
